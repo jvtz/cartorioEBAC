612e958a6a24c665b76d3ddec695f30c63bf2246
João Victor T Zamfirov <jvterenciano@protonmail.com>
2023-09-19 17:47:41 -0300

    Saída do sistema
    Nova opção de saída do sistema.

--- main.c
+++ main.c
@@ -131,6 +131,11 @@ int main() {
             case 3:
                 deletar();
                 break;
+                
+            case 4:
+                printf("Obrigado por utilizar o sistema!\n");
+                return 0;
+                break;
 
             default:
                 printf("Essa opção não está disponível.\n");
